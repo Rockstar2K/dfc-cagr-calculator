495223bb708c2a25ea66a951fc6712e74dddcc66
rocofernandezpagella@gmail.com <46428180+Rockstar2K@users.noreply.github.com>
2024-09-25 19:16:37 +0700

    history works

--- script.js
+++ script.js
@@ -3,10 +3,7 @@ let discountRateSelected = 0;
 let yearsSelected = 0;
 let initialInvestment = 0;
 
-
 let currentStockPrice = 0;
-
-
 
 class Company {
   constructor(name, cashFlow, inBillions, stockName) {
@@ -29,13 +26,12 @@ document
 
 //* Calculate
 async function getSelectedOptions() {
-
+  let companyName = "";
   let cashFlow = 0;
-  let yearsSelected = 0; 
+  let yearsSelected = 0;
   let discountRateSelected = 0;
   let initialInvestment = 0;
-  let stock = ';'
-
+  let stock = ";";
 
   const companySelect = document.getElementById("companySelect");
   const companySelectedValue = companySelect.value;
@@ -44,8 +40,10 @@ async function getSelectedOptions() {
   const selectedCompany = companies.find(
     (company) => company.name === companySelectedValue
   );
+  companyName = selectedCompany.name;
+
   console.log("selectedCompany: " + selectedCompany.name);
-  cashFlow = parseInt(selectedCompany.cashFlow, 10)
+  cashFlow = parseInt(selectedCompany.cashFlow, 10);
 
   const yearsSelect = document.getElementById("yearsSelect");
   yearsSelected = parseInt(yearsSelect.value, 10);
@@ -57,7 +55,6 @@ async function getSelectedOptions() {
   initialInvestment = parseInt(initialInvestmentSelect.value);
 
   stock = selectedCompany.stockName;
-
 
   console.log(
     " selectedCompany: " +
@@ -79,17 +76,96 @@ async function getSelectedOptions() {
 
   let endValue = dfcFormula(discountRateSelected, yearsSelected, cashFlow);
   let cagrPercentageResult = getCAGR(cashFlow, endValue, yearsSelected);
-  let finalInvestmentResult = getFinalInvestment(cagrPercentageResult, initialInvestment, yearsSelected);
+  let finalInvestmentResult = getFinalInvestment(
+    cagrPercentageResult,
+    initialInvestment,
+    yearsSelected
+  );
 
-  await getStockData(stock);
+  //await getStockData(stock);
 
   let futureStockPrice = stockPriceCagrProjection(cagrPercentageResult);
-  updateChart(stock);
-  //updateChart(stock, futureStockPrice);
+  //updateChart(stock);
+
+  //!updateChart(stock, futureStockPrice);
   displayResult(cagrPercentageResult, initialInvestment, finalInvestmentResult);
+
+  saveToHistory(
+    companyName,
+    yearsSelected,
+    discountRateSelected,
+    initialInvestment,
+    finalInvestmentResult,
+    cagrPercentageResult
+  );
+  showHistory();
 }
 
-//* Final Investment Calculation
+//* save to history
+let searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
+
+function saveToHistory(
+  selectedCompany,
+  yearsSelected,
+  discountRateSelected,
+  initialInvestment,
+  finalInvestment,
+  cagrPercentageResult
+) {
+  let search = {
+    companyHistory: selectedCompany,
+    yearsHistory: yearsSelected,
+    initialInvestmentHistory: initialInvestment,
+    discountRateHistory: discountRateSelected,
+    finalInvestmentHistory: finalInvestment,
+    cagrHistory: cagrPercentageResult,
+  };
+
+  searchHistory.push(search);
+
+  localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
+
+  console.log("Updated search history:", searchHistory);
+}
+
+const toggleBtn = document.getElementById("toggleHistoryBtn");
+const toggleContent = document.getElementById("toggleHistoryContent");
+
+toggleBtn.addEventListener("click", function () {
+  // Toggle the visibility of the content
+  if (toggleContent.style.display === "none") {
+    toggleContent.style.display = "block";
+    toggleBtn.textContent = "Hide History"; // Change button text
+  } else {
+    toggleContent.style.display = "none";
+    toggleBtn.textContent = "Show History"; // Change button text
+  }
+});
+
+showHistory();
+function showHistory() {
+  
+  if (searchHistory.length > 0) { //add to history toggle
+
+    toggleContent.innerHTML = "";
+
+    searchHistory.forEach((search, index) => {
+      const searchItem = `
+        <div>
+          <p>${search.companyHistory} <strong>#${index + 1}</strong></p>
+          <p>Years: ${search.yearsHistory}</p>
+          <p>Initial Investment: <strong>${search.initialInvestmentHistory} USD</strong></p>
+          <p>Discount Rate: <strong>${search.discountRateHistory} %</strong></p>
+          <p>Final Investment: <strong>${search.finalInvestmentHistory.toFixed(0)} USD</strong></p>
+          <p>CAGR: <strong>${search.cagrHistory}%</strong></p>
+        </div><hr>
+      `;
+      toggleContent.innerHTML += searchItem;
+    });
+  } else {
+    toggleContent.innerHTML = "<p>No search history available.</p>";
+  }
+}
 
 //* Final Investment Calculation
 function getFinalInvestment(cagrResult, initialInvestment, yearsSelected) {
@@ -99,13 +175,14 @@ function getFinalInvestment(cagrResult, initialInvestment, yearsSelected) {
   console.log("Initial Investment: " + finalInvestment + " USD");
 
   for (let i = 1; i <= yearsSelected; i++) {
-    finalInvestment *= (1 + cagrDecimal); // Compound the investment each year
-    console.log("Investment year " + i + ": " + finalInvestment.toFixed(2) + " USD");
+    finalInvestment *= 1 + cagrDecimal; // Compound the investment each year
+    console.log(
+      "Investment year " + i + ": " + finalInvestment.toFixed(2) + " USD"
+    );
   }
 
   return finalInvestment; // Return the final investment after all years
 }
-
 
 //* Display result
 
@@ -116,27 +193,28 @@ function displayResult(
 ) {
   let cagrClass;
   if (cagrPercentageResult >= 25) {
-    cagrClass = 'green'; // Green
+    cagrClass = "green"; // Green
   } else if (cagrPercentageResult >= 10) {
-    cagrClass = 'yellow'; // Yellow
+    cagrClass = "yellow"; // Yellow
   } else if (cagrPercentageResult < 10) {
-    cagrClass = 'red'; // Red
-  } 
+    cagrClass = "red"; // Red
+  }
 
   let finalInvestmentClass;
-  if (finalInvestmentResult > initialInvestment){
-    finalInvestmentClass = 'green'
-  } else{
-    finalInvestmentClass = 'red'
+  if (finalInvestmentResult > initialInvestment) {
+    finalInvestmentClass = "green";
+  } else {
+    finalInvestmentClass = "red";
   }
 
   document.getElementById("display-investment").innerHTML = `
     <p id="display-investment_initial-investment"> Initial Investment: </br><b>${initialInvestment} USD </b></p>
-    <p id="display-investment_final-investment"> Final Investment: </br><b class="${finalInvestmentClass}">${finalInvestmentResult.toFixed(0)} USD </b></p>
+    <p id="display-investment_final-investment"> Final Investment: </br><b class="${finalInvestmentClass}">${finalInvestmentResult.toFixed(
+    0
+  )} USD </b></p>
     <p id="display-investment_cagr"> CAGR: </br><b class="${cagrClass}">${cagrPercentageResult} %</b></p>
   `;
 }
-
 
 //* Stock Price projected with CAGR
 
@@ -144,19 +222,16 @@ function stockPriceCagrProjection(cagr) {
   try {
     cagr = cagr / 100;
     projectedStockPrice = currentStockPrice; // Start with current price
-  
+
     for (let i = 1; i <= yearsSelected; i++) {
-        projectedStockPrice *= (1 + cagr); // Update with compounding
+      projectedStockPrice *= 1 + cagr; // Update with compounding
     }
-    
-  
+
     console.log("FINAL STOCK PRICE: " + projectedStockPrice);
     return projectedStockPrice;
-    
   } catch (error) {
     console.log(error);
   }
- 
 }
 
 //*DFC
@@ -167,14 +242,13 @@ function dfcFormula(discountRateSelected, yearsSelected, cashFlow) {
 
   for (let i = 1; i <= yearsSelected; i++) {
     DFC = DFC + cashFlow / (1 + discountRatePercentage) ** i; // cash flow year / (1 + discount rate) elevated to iteration
-    console.log('DFC inside for year '+ i + ': ' +  DFC);
+    console.log("DFC inside for year " + i + ": " + DFC);
   }
 
   return DFC;
 }
 
 function getCAGR(startValue, endValue, yearsSelected) {
-
   let cagr = (endValue / startValue) ** (1 / yearsSelected) - 1;
   let cagrPercentage = (cagr * 100).toFixed(2);
 
@@ -255,41 +329,37 @@ function storeRightStockData(stockSymbol, data) {
 
 function updateChart(stockSymbol) {
   try {
+    const dates = stockData[stockSymbol].dates;
+    const closePrices = stockData[stockSymbol].closePrices;
 
-
-  const dates = stockData[stockSymbol].dates;
-  const closePrices = stockData[stockSymbol].closePrices;
-
-  const ctx = document.getElementById("myChart").getContext("2d");
-  const myChart = new Chart(ctx, {
-    type: "line",
-    data: {
-      labels: dates, // Dates as x-axis labels
-      datasets: [
-        {
-          label: `${stockSymbol.toUpperCase()} Monthly Close Prices`,
-          data: closePrices, // Close prices as the data
-          borderColor: "rgba(75, 192, 192, 1)",
-          backgroundColor: "rgba(75, 192, 192, 0.2)",
-          fill: false,
-          tension: 0.1,
-        },
-      ],
-    },
-    options: {
-      scales: {
-        x: {
-          type: "time",
-          time: {
-            unit: "month",
+    const ctx = document.getElementById("myChart").getContext("2d");
+    const myChart = new Chart(ctx, {
+      type: "line",
+      data: {
+        labels: dates, // Dates as x-axis labels
+        datasets: [
+          {
+            label: `${stockSymbol.toUpperCase()} Monthly Close Prices`,
+            data: closePrices, // Close prices as the data
+            borderColor: "rgba(75, 192, 192, 1)",
+            backgroundColor: "rgba(75, 192, 192, 0.2)",
+            fill: false,
+            tension: 0.1,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          x: {
+            type: "time",
+            time: {
+              unit: "month",
+            },
           },
         },
       },
-    },
-  });
-    
+    });
   } catch (error) {
     console.log(error);
   }
-  
 }
